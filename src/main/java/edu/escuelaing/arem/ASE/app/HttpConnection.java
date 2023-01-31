525085a4e077a5bbe86550df9616b48199f4a2cf
User <sergioaotero26@hotmail.com>
2023-01-31 00:32:54 -0500

    medio sirve esta joda

---
+++ src/main/java/edu/escuelaing/arem/ASE/app/HttpConnection.java
@@ -0,0 +1,43 @@
+package edu.escuelaing.arem.ASE.app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpConnection {
+
+    private static final String USER_AGENT = "Mozilla/5.0";
+
+    public String searchOnAPI(String GET_URL) throws IOException {
+
+        URL obj = new URL(GET_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.print(response.toString());
+            return response.toString();
+        } else {
+            return "GET request not worked";
+        }
+    }
+}
